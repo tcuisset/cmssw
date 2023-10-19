467b1c4df41d53683eade2b45acc4700b693258e
Théo <theo.cuisset@polytechnique.edu>
2023-10-19 10:24:26 +0000

    Move feature normalization to graph

--- RecoHGCal/TICL/python/superclusteringTf_cfi.py
+++ RecoHGCal/TICL/python/superclusteringTf_cfi.py
@@ -1,5 +1,5 @@
 from PhysicsTools.TensorFlow.tfGraphDefProducer_cfi import tfGraphDefProducer as _tfGraphDefProducer
 superclusteringTf = _tfGraphDefProducer.clone(
     ComponentName = "superclusteringTf",
-    FileName = "RecoHGCal/TICL/data/tf_models/supercls_v1.pb"
+    FileName = "RecoHGCal/TICL/data/tf_models/supercls_v2.pb"
 )
