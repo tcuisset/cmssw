bf4252f454db112e23878ab7c10bcf78e82cb7d9
Théo <theo.cuisset@polytechnique.edu>
2023-11-02 13:53:42 +0000

    Alessandro v2 model

--- RecoHGCal/TICL/plugins/SuperclusteringProducer.cc
+++ RecoHGCal/TICL/plugins/SuperclusteringProducer.cc
@@ -1,6 +1,11 @@
 #include <numeric>      // std::iota
 #include <algorithm>    // std::sort, std::stable_sort
 
+#include <Math/Vector2D.h>
+#include <Math/Vector3D.h>
+#include <Math/Rotation3D.h>
+#include <Math/VectorUtil.h>
+
 #include "FWCore/Framework/interface/stream/EDProducer.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/ParameterSet/interface/ConfigurationDescriptions.h"
@@ -41,6 +46,7 @@ private:
   //const edm::EDGetTokenT<std::vector<reco::CaloCluster>> clusters_token_;
 
   const edm::ESGetToken<TfGraphDefWrapper, TfGraphRecord> tfDnnToken_; // ES Token to obtain tensorflow session and graph
+  const std::string dnnVersion_; 
   float nnWorkingPoint_; // Working point for neural network (above this score, consider the trackster candidate for superclustering)
 };
 
@@ -49,6 +55,7 @@ SuperclusteringProducer::SuperclusteringProducer(const edm::ParameterSet &ps)
     : tracksters_clue3d_token_(consumes<std::vector<Trackster>>(ps.getParameter<edm::InputTag>("trackstersclue3d"))),
       //clusters_token_(consumes<std::vector<reco::CaloCluster>>(ps.getParameter<edm::InputTag>("layer_clusters"))),
       tfDnnToken_(esConsumes(edm::ESInputTag("", ps.getParameter<std::string>("tfDnnLabel")))),
+      dnnVersion_(ps.getParameter<std::string>("dnnVersion")),
       nnWorkingPoint_(ps.getParameter<double>("nnWorkingPoint")) {
   produces<SuperclusteringResult>("superclusteredTracksters"); // list of sets of indices of tracksters corresponding to a multicluster
 }
@@ -60,42 +67,25 @@ void SuperclusteringProducer::endJob(){};
 void SuperclusteringProducer::beginRun(edm::Run const &iEvent, edm::EventSetup const &es) {
 };
 
-
-/* class NNInput {
+class AbstractDNNInput {
 public:
-  const int featureCount;
-  void fillFeatures(Eigen::TensorMap<Eigen::Tensor<float, 2, 1, Eigen::DenseIndex>, 16> eigenTensor, int batchIndex, Trackster const& ts_base, Trackster const& ts_toCluster) = 0;
-}; */
+  virtual ~AbstractDNNInput() {};
+  virtual int featureCount() const = 0;
+  virtual void fillFeatures(tensorflow::Tensor& inputTensor, int batchIndex, Trackster const& ts_base, Trackster const& ts_toCluster) = 0;
+};
 
-class PlaceholderNNInput{
+/*
+DNN by Alessandro Tarabini comes in 2 versions with the following observables : 
+ 'v1_dEtadPhi': ['DeltaEta', 'DeltaPhi', 'multi_en', 'multi_eta', 'multi_pt', 'seedEta','seedPhi','seedEn', 'seedPt'],
+  'v2_dEtadPhi': ['DeltaEta', 'DeltaPhi', 'multi_en', 'multi_eta', 'multi_pt', 'seedEta','seedPhi','seedEn', 'seedPt', 'theta', 'theta_xz_seedFrame', 'theta_yz_seedFrame', 'theta_xy_cmsFrame', 'theta_yz_cmsFrame', 'theta_xz_cmsFrame', 'explVar', 'explVarRatio'],
+*/
+
+class DNNInputAlessandroV1 : public AbstractDNNInput {
 public:
-  PlaceholderNNInput() {};
+  int featureCount() const override { return 9; }
 
-  static constexpr int featureCount{9};
-  /*
-  // SCaling values for network input, taken from Alessandro's notebook
-  static constexpr std::array<float, featureCount>  scalingFactorTensor{{5.009924761018587,
-  1.0018188597242355,
-  0.010292174692060528,
-  0.16288265085698453,
-  0.04559109321673852,
-  0.1682473240927026,
-  0.15967887456557378,
-  0.0015586544595969428,
-  0.010722259319073196}};
-  static constexpr std::array<float, featureCount> scalingMinTensor{{0.49977842782898685,
-  0.4994941706919238,
-  -0.020606080641338675,
-  0.5002919753099604,
-  -0.009400341457049127,
-  0.49997065712986616,
-  0.4996155459607123,
-  -0.033067499716928135,
-  -0.05139944510782373}};
-  */
 //Eigen::TensorFixedSize<float, Eigen::Sizes<featureCount>>
-  void fillFeatures(tensorflow::Tensor& inputTensor, int batchIndex, Trackster const& ts_base, Trackster const& ts_toCluster) {
-    //float deltaEta = (ts_toCluster.eigenvectors(0) - ts_base.eigenvectors(0)).Eta();
+  void fillFeatures(tensorflow::Tensor& inputTensor, int batchIndex, Trackster const& ts_base, Trackster const& ts_toCluster) override {
     /*  We use the barycenter for most of the variables below as that is what seems to have been used by Alessandro Tarabini, 
       but using PCA might be better. 
      (It would need retraining of the DNN)
@@ -112,13 +102,85 @@ public:
     eigenTensor(batchIndex, 6) = ts_base.barycenter().Phi(); //seedPhi
     eigenTensor(batchIndex, 7) = ts_base.raw_energy(); //seedEn
     eigenTensor(batchIndex, 8) = (ts_base.raw_energy() * std::sin(ts_toCluster.barycenter().Theta())); //seedPt
+  }
+};
 
-    // TODO replace this with proper matrix multiplication (probably outside trackster loop)
-    // or better : put inside tensorflow graph
-    /*for (int i = 0; i < featureCount; i++) {
-      eigenTensor(batchIndex, i) *= scalingFactorTensor[i];
-      eigenTensor(batchIndex, i) += scalingMinTensor[i];
-    }*/
+/*
+//class CMSCoordinateTag {};
+class SeedCoordinateTag {};
+
+using Vector3Seed = ROOT::Math::DisplacementVector3D<ROOT::Math::Cartesian3D<float>, SeedCoordinateTag>;
+using Vector3CMS = ROOT::Math::XYZVectorF;
+using Vector2Seed = ROOT::Math::DisplacementVector2D<ROOT::Math::Cartesian3D<float>, SeedCoordinateTag>;
+using Vector2CMS = ROOT::Math::XYVectorF;
+*/
+
+template <class Vector1, class Vector2>
+double CosTheta2D( const Vector1 &  v1, const Vector2  & v2) {
+  double arg;
+  double v1_r2 = v1.X()*v1.X() + v1.Y()*v1.Y();
+  double v2_r2 = v2.X()*v2.X() + v2.Y()*v2.Y();
+  double ptot2 = v1_r2*v2_r2;
+  if(ptot2 <= 0) {
+      arg = 0.0;
+  }else{
+      double pdot = v1.X()*v2.X() + v1.Y()*v2.Y();
+      using std::sqrt;
+      arg = pdot/sqrt(ptot2);
+      if(arg >  1.0) arg =  1.0;
+      if(arg < -1.0) arg = -1.0;
+  }
+  return arg;
+}
+template <class Vector1, class Vector2>
+inline double Angle2D( const  Vector1 & v1, const Vector2 & v2) {
+  return std::acos( CosTheta2D(v1, v2) );
+}
+
+class DNNInputAlessandroV2 : public AbstractDNNInput{
+public:
+  int featureCount() const override { return 17; }
+
+//Eigen::TensorFixedSize<float, Eigen::Sizes<featureCount>>
+  void fillFeatures(tensorflow::Tensor& inputTensor, int batchIndex, Trackster const& ts_base, Trackster const& ts_toCluster) override {
+    /*  We use the barycenter for most of the variables below as that is what seems to have been used by Alessandro Tarabini, 
+      but using PCA might be better. 
+     (It would need retraining of the DNN)
+    */
+    assert(inputTensor.dims() == 2 && inputTensor.dim_size(1) == featureCount());
+    assert(batchIndex < inputTensor.dim_size(0));
+    auto eigenTensor = inputTensor.tensor<float, 2>();
+
+    using ROOT::Math::VectorUtil::Angle;
+    using ROOT::Math::XYZVectorF;
+    using ROOT::Math::XYVectorF;
+    XYZVectorF const& pca_base_cmsFrame(ts_base.eigenvectors(0));
+    XYZVectorF const& pca_cand_cmsFrame(ts_toCluster.eigenvectors(0));
+    XYZVectorF xs(pca_base_cmsFrame.Cross(XYZVectorF(0, 0, 1)).Unit());
+    ROOT::Math::Rotation3D rot(
+        xs, 
+        xs.Cross(pca_base_cmsFrame).Unit(),
+        pca_base_cmsFrame);
+      
+    XYZVectorF pca_cand_seedFrame = rot(pca_cand_cmsFrame); // seed coordinates
+
+    eigenTensor(batchIndex, 0) = std::abs(ts_toCluster.barycenter().Eta() - ts_base.barycenter().Eta());; //DeltaEtaBaryc
+    eigenTensor(batchIndex, 1) = std::abs(ts_toCluster.barycenter().Phi() - ts_base.barycenter().phi());; //DeltaPhiBaryc
+    eigenTensor(batchIndex, 2) = ts_toCluster.raw_energy(); //multi_en
+    eigenTensor(batchIndex, 3) = ts_toCluster.barycenter().Eta(); //multi_eta
+    eigenTensor(batchIndex, 4) = (ts_toCluster.raw_energy() * std::sin(ts_toCluster.barycenter().Theta())); //multi_pt
+    eigenTensor(batchIndex, 5) = ts_base.barycenter().Eta(); //seedEta
+    eigenTensor(batchIndex, 6) = ts_base.barycenter().Phi(); //seedPhi
+    eigenTensor(batchIndex, 7) = ts_base.raw_energy(); //seedEn
+    eigenTensor(batchIndex, 8) = (ts_base.raw_energy() * std::sin(ts_toCluster.barycenter().Theta())); //seedPt
+    eigenTensor(batchIndex, 9) = Angle(pca_cand_cmsFrame, pca_base_cmsFrame); // theta : angle between seed and candidate
+    eigenTensor(batchIndex, 10) = Angle2D(XYVectorF(pca_cand_seedFrame.y(), pca_cand_seedFrame.z()), XYVectorF(0, 1)); // theta_xz_seedFrame
+    eigenTensor(batchIndex, 11) = Angle2D(XYVectorF(pca_cand_seedFrame.y(), pca_cand_seedFrame.z()), XYVectorF(0, 1)); // theta_yz_seedFrame
+    eigenTensor(batchIndex, 12) = Angle2D(XYVectorF(pca_cand_cmsFrame.x(), pca_cand_cmsFrame.y()), XYVectorF(pca_base_cmsFrame.x(), pca_base_cmsFrame.y())); // theta_xy_cmsFrame
+    eigenTensor(batchIndex, 13) = Angle2D(XYVectorF(pca_cand_cmsFrame.y(), pca_cand_cmsFrame.z()), XYVectorF(pca_base_cmsFrame.y(), pca_base_cmsFrame.z())); // theta_yz_cmsFrame
+    eigenTensor(batchIndex, 14) = Angle2D(XYVectorF(pca_cand_cmsFrame.x(), pca_cand_cmsFrame.z()), XYVectorF(pca_base_cmsFrame.x(), pca_base_cmsFrame.z())); // theta_xz_cmsFrame
+    eigenTensor(batchIndex, 15) = ts_toCluster.eigenvalues()[0]; // explVar
+    eigenTensor(batchIndex, 16) = ts_toCluster.eigenvalues()[0] / std::accumulate(std::begin(ts_toCluster.eigenvalues()), std::end(ts_toCluster.eigenvalues()), 0, std::plus<float>()); // explVarRatio
   }
 };
 
@@ -131,8 +193,13 @@ void SuperclusteringProducer::produce(edm::Event &evt, const edm::EventSetup &es
   const std::size_t tracksterCount = inputTracksters->size();
   const int mainBatchSize = tracksterCount * (tracksterCount - 1);
 
-  PlaceholderNNInput nnInput;
-  const int feature_count = nnInput.featureCount;
+  std::unique_ptr<AbstractDNNInput> nnInput;
+  if (dnnVersion_ == "alessandro-v1")
+    nnInput = std::make_unique<DNNInputAlessandroV1>();
+  else if (dnnVersion_ == "alessandro-v2")
+    nnInput = std::make_unique<DNNInputAlessandroV2>();
+
+  const int feature_count = nnInput->featureCount();
   tensorflow::Tensor inputTensor(tensorflow::DT_FLOAT, {mainBatchSize, feature_count});
 
   //Sorting tracksters by decreasing order of pT
@@ -150,7 +217,7 @@ void SuperclusteringProducer::produce(edm::Event &evt, const edm::EventSetup &es
         if (trackstersIndicesPt[ts_base_idx] != ts_toCluster_idx) { // Don't supercluster trackster with itself
             Trackster const& ts_toCluster = (*inputTracksters)[ts_toCluster_idx]; // no need to sort by pt here
 
-            nnInput.fillFeatures(inputTensor, ts_base_idx*(tracksterCount-1) + ts_toCluster_idx_tensor, ts_base, ts_toCluster);
+            nnInput->fillFeatures(inputTensor, ts_base_idx*(tracksterCount-1) + ts_toCluster_idx_tensor, ts_base, ts_toCluster);
             ts_toCluster_idx_tensor++; 
         }
     }
@@ -222,6 +289,7 @@ void SuperclusteringProducer::produce(edm::Event &evt, const edm::EventSetup &es
 void SuperclusteringProducer::fillDescriptions(edm::ConfigurationDescriptions &descriptions) {
   edm::ParameterSetDescription desc;
   desc.add<std::string>("tfDnnLabel", "superclusteringTf");
+  desc.add<std::string>("dnnVersion", "alessandro-v1");
   desc.add<edm::InputTag>("trackstersclue3d", edm::InputTag("ticlTrackstersCLUE3DHigh"));
   desc.add<double>("nnWorkingPoint", 0.51);
   descriptions.add("superclusteringProducer", desc);
