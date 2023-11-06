f12de56674979654c845b8f68819074f9a986f55
Théo <theo.cuisset@polytechnique.edu>
2023-11-06 17:25:46 +0000

    Attempt to store DNN score in TICLDumper as a truncated float. Does not work as ROOT does not seem to support truncated float inside STL collections (even when putting it inside a class)

--- DataFormats/HGCalReco/interface/Common.h
+++ DataFormats/HGCalReco/interface/Common.h
@@ -1,8 +1,11 @@
 #ifndef DataFormats_HGCalReco_Common_h
 #define DataFormats_HGCalReco_Common_h
 
+#include <RtypesCore.h>
+
 #include <vector>
 #include <array>
+#include <tuple>
 #include <cstdint>
 
 namespace ticl {
@@ -30,8 +33,17 @@ namespace ticl {
 
 namespace ticl {
   typedef std::vector<std::pair<unsigned int, float> > TICLClusterFilterMask;
-  typedef std::vector<std::vector<std::size_t>> SuperclusteringResult;
-  typedef std::vector<std::vector<float>> SuperclusteringDNNScore;
+  typedef std::vector<std::vector<std::size_t>> SuperclusteringResult; // Each inner vector is a list of trackster ids to supercluster
+  struct SuperclusteringDNNScoreElement {
+    std::size_t tsSeedId_; // Trackster id of the seed
+    std::size_t tsCandId_; // Trackster id of the candidate to be superclustered with the seed
+    Float16_t dnnScore_; // score of the dnn
+
+    SuperclusteringDNNScoreElement(std::size_t tsSeedId=0, std::size_t tsCandId=0, float dnnScore=0) :
+      tsSeedId_(tsSeedId), tsCandId_(tsCandId), dnnScore_(dnnScore)
+    {}
+  };
+  typedef std::vector<SuperclusteringDNNScoreElement> SuperclusteringDNNScore; 
 }  // namespace ticl
 
 #endif  // DataFormats_HGCalReco_Common_h
